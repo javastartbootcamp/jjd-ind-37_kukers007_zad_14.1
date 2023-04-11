f975d325af74dc7609863c02fca93eb7455833a9
kukers007 <96911087+kukers007@users.noreply.github.com>
2023-04-11 20:12:38 +0200

    Solution 14.1 poprawka 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,14 +15,12 @@ public class Main {
                 number = scanner.nextInt();
                 numbers.put(number, numbers.getOrDefault(number, 0) + 1);
             }
-            scanner.close();
-
+            for (Map.Entry<Integer, Integer> entry : numbers.entrySet()) {
+                System.out.println(entry.getKey() + " - liczba wystąpień " + entry.getValue());
+                scanner.close();
+            }
         } catch (FileNotFoundException ex) {
-            throw new RuntimeException(ex);
-        }
-        Map<Integer, Integer> sortNumbers = new TreeMap<>(numbers);
-        for (Map.Entry<Integer, Integer> entry : sortNumbers.entrySet()) {
-            System.out.println(entry.getKey() + " - liczba wystąpień " + entry.getValue());
+            System.err.println("File not found");
         }
     }
 }
